e54dd82d9d66f016faa029a55462b1d3a5016f54
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-08-26 08:07:39 +0530

    project page

--- frontend/src/Pages/Projects.jsx
+++ frontend/src/Pages/Projects.jsx
@@ -24,76 +24,114 @@ const Projects = () => {
           CREATE NEW PROJECT
         </Button>
       </HStack>
-      <HStack gap={"8"} mt="9" bg="#fff" p="3" border={"1px solid #ccced0"}>
-        <Flex alignItems={"center"} gap="1">
-          {" "}
-          <Text color={"grey"}>FILTER</Text>
-        </Flex>
-        <Flex alignItems={"center"} gap="1">
-          {" "}
-          <Text>Active</Text> <ChevronDownIcon fontSize={"lg"} />
-        </Flex>
-        <Flex alignItems={"center"} gap="1">
-          {" "}
-          <Text>Client</Text> <ChevronDownIcon fontSize={"lg"} />
-        </Flex>
-        <Flex alignItems={"center"} gap="1">
-          {" "}
-          <Text>Access</Text> <ChevronDownIcon fontSize={"lg"} />
-        </Flex>
-        <Flex alignItems={"center"} gap="1">
-          {" "}
-          <Text>Billing</Text> <ChevronDownIcon fontSize={"lg"} />
-        </Flex>
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents="none"
-            children={<SearchIcon color="gray.300" />}
-          />
-          <Input type="text" outLine="#ccced0" placeholder="Project name" />
-        </InputGroup>
-        <Button
-          border="1px solid #03A9F4"
-          color={"#03A9F4"}
-          bg={"transparent"}
-          _hover={{ bg: "transparent" }}
-          pl={9}
-          pr={9}
-        >
-          APPLY FILTER
-        </Button>
-      </HStack>
+      <Grid
+        gap={"8"}
+        mt="9"
+        bg="#fff"
+        p="3"
+        border={"1px solid #ccced0"}
+        justifyContent="center"
+        templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(2, 1fr)"]}
+      >
+        <Grid templateColumns={["repeat(2, 1fr)", "repeat(4, 1fr)"]} gap={14}>
+          <Flex
+            alignItems={"center"}
+            gap="1"
+            display={["none", "none", "block"]}
+          >
+            {" "}
+            <Text color={"grey"}>FILTER</Text>
+          </Flex>
+          <Flex alignItems={"center"} gap="1">
+            {" "}
+            <Text>Active</Text> <ChevronDownIcon fontSize={"lg"} />
+          </Flex>
+          <Flex alignItems={"center"} gap="1">
+            {" "}
+            <Text>Client</Text> <ChevronDownIcon fontSize={"lg"} />
+          </Flex>
+          <Flex alignItems={"center"} gap="1">
+            {" "}
+            <Text>Access</Text> <ChevronDownIcon fontSize={"lg"} />
+          </Flex>
+          <Flex alignItems={"center"} gap="1">
+            {" "}
+            <Text>Billing</Text> <ChevronDownIcon fontSize={"lg"} />
+          </Flex>
+        </Grid>
+        <HStack>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input type="text" outLine="#ccced0" placeholder="Project name" />
+          </InputGroup>
+          <Button
+            border="1px solid #03A9F4"
+            color={"#03A9F4"}
+            bg={"transparent"}
+            _hover={{ bg: "transparent" }}
+            pl={9}
+            pr={9}
+            display={["none", "block", "block"]}
+          >
+            APPLY FILTER
+          </Button>
+          <Button
+            border="1px solid #03A9F4"
+            color={"#03A9F4"}
+            bg={"transparent"}
+            _hover={{ bg: "transparent" }}
+            pl={9}
+            pr={9}
+            display={["block", "none", "none"]}
+          >
+            APPLY
+          </Button>
+        </HStack>
+      </Grid>
       <Grid mt="8">
         <Flex bg="#e4eaee" p="3">
           <Text>Projects</Text>
         </Flex>
-       <HStack bg="#fff" justifyContent={"space-between"} pr="14">
-       <Flex  p="3" alignItems={"center"} gap={2}>
-          <Checkbox />
-          <Text>Name</Text>
-          <UpDownIcon fontSize={"12"} />
-        </Flex>
-        <Flex  p="3" alignItems={"center"} gap={2}>
-       
-          <Text>CLIENT</Text>
-          <UpDownIcon fontSize={"12"} />
-        </Flex>
-        <Flex  p="3" alignItems={"center"} gap={2}>
-       
-          <Text>AMOUNT</Text>
-          <UpDownIcon fontSize={"12"} />
-        </Flex>
-        <Flex  p="3" alignItems={"center"} gap={2}>
-       
-          <Text>PROGRESS</Text>
-          <UpDownIcon fontSize={"12"} />
-        </Flex>
-        <Flex  p="3" alignItems={"center"} gap={2}>
-       
-          <Text>ACCESS</Text>
-          <UpDownIcon fontSize={"12"} />
-        </Flex>
-       </HStack>
+        <HStack bg="#fff" justifyContent={"space-between"} pr="14">
+          <Flex p="3" alignItems={"center"} gap={2}>
+            <Checkbox />
+            <Text>Name</Text>
+            <UpDownIcon fontSize={"12"} />
+          </Flex>
+
+          <Box display={["none", "none", "block"]}>
+            <Flex p="3" alignItems={"center"} gap={2}>
+              <Text>CLIENT</Text>
+              <UpDownIcon fontSize={"12"} />
+            </Flex>
+          </Box>
+          <Box display={["none", "none", "block"]}>
+            <Flex p="3" alignItems={"center"} gap={2}>
+              <Text>AMOUNT</Text>
+              <UpDownIcon fontSize={"12"} />
+            </Flex>
+          </Box>
+          <Box display={["none", "none", "block"]}>
+            {" "}
+            <Flex p="3" alignItems={"center"} gap={2}>
+              <Text>PROGRESS</Text>
+              <UpDownIcon fontSize={"12"} />
+            </Flex>
+          </Box>
+
+          <Box display={["none", "none", "block"]}>
+            <Flex p="3" alignItems={"center"} gap={2}>
+              <Text>ACCESS</Text>
+              <UpDownIcon fontSize={"12"} />
+            </Flex>
+          </Box>
+          <Box display={["block", "block", "none"]}>
+            <ChevronDownIcon />
+          </Box>
+        </HStack>
       </Grid>
     </Box>
   );
