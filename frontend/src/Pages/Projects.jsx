d66fee9a99c6e35aeaa1c4452faedf3838b23cdf
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-08-26 09:26:55 +0530

    fetch

--- frontend/src/Pages/Projects.jsx
+++ frontend/src/Pages/Projects.jsx
@@ -13,6 +13,7 @@ import {
   Checkbox,
 } from "@chakra-ui/react";
 import { SearchIcon, ChevronDownIcon, UpDownIcon } from "@chakra-ui/icons";
+import { useState, useEffect } from "react";
 const Projects = () => {
   return (
     <Box>
@@ -20,8 +21,11 @@ const Projects = () => {
         <Heading fontWeight={"400"} size={["md", "lg"]} color={"gray.600"}>
           Projects
         </Heading>
-        <Button color="#fff" _hover={{ bg: "blue.400" }} bgColor={"#03A9F4"}>
+        <Button color="#fff" display={["none","none","block"]} _hover={{ bg: "blue.400" }} bgColor={"#03A9F4"}>
           CREATE NEW PROJECT
+        </Button>
+        <Button display={["block","block","none"]} color="#fff" _hover={{ bg: "blue.400" }} bgColor={"#03A9F4"}>
+          CREATE NEW 
         </Button>
       </HStack>
       <Grid
@@ -30,31 +34,47 @@ const Projects = () => {
         bg="#fff"
         p="3"
         border={"1px solid #ccced0"}
-        justifyContent="center"
         templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(2, 1fr)"]}
+        
+       
       >
-        <Grid templateColumns={["repeat(2, 1fr)", "repeat(4, 1fr)"]} gap={14}>
-          <Flex
-            alignItems={"center"}
-            gap="1"
-            display={["none", "none", "block"]}
+        <Grid
+          templateColumns={[
+            "repeat(2, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(5, 1fr)",
+          ]}
+          gap={14}
+       
+        >
+          <Flex gap="1" display={["none", "none", "block"]}
+         
           >
             {" "}
             <Text color={"grey"}>FILTER</Text>
           </Flex>
-          <Flex alignItems={"center"} gap="1">
+          <Flex 
+           
+           justifyContent="center"
+          alignItems={"center"} gap="1">
             {" "}
             <Text>Active</Text> <ChevronDownIcon fontSize={"lg"} />
           </Flex>
-          <Flex alignItems={"center"} gap="1">
+          <Flex 
+          justifyContent="center"
+          alignItems={"center"} gap="1">
             {" "}
             <Text>Client</Text> <ChevronDownIcon fontSize={"lg"} />
           </Flex>
-          <Flex alignItems={"center"} gap="1">
+          <Flex 
+          justifyContent="center"
+          alignItems={"center"} gap="1">
             {" "}
             <Text>Access</Text> <ChevronDownIcon fontSize={"lg"} />
           </Flex>
-          <Flex alignItems={"center"} gap="1">
+          <Flex 
+          justifyContent="center"
+          alignItems={"center"} gap="1">
             {" "}
             <Text>Billing</Text> <ChevronDownIcon fontSize={"lg"} />
           </Flex>
@@ -72,9 +92,11 @@ const Projects = () => {
             color={"#03A9F4"}
             bg={"transparent"}
             _hover={{ bg: "transparent" }}
-            pl={9}
+            pl={7}
             pr={9}
             display={["none", "block", "block"]}
+            textAlign="center"
+            alignItems="center"
           >
             APPLY FILTER
           </Button>
@@ -86,6 +108,8 @@ const Projects = () => {
             pl={9}
             pr={9}
             display={["block", "none", "none"]}
+            textAlign="center"
+            alignItems="center"
           >
             APPLY
           </Button>
@@ -132,8 +156,70 @@ const Projects = () => {
             <ChevronDownIcon />
           </Box>
         </HStack>
+        <DataFetch />
       </Grid>
     </Box>
   );
 };
 export default Projects;
+const DataFetch = () => {
+  const [DataFetch, setDataFetch] = useState();
+  const FakeData = [
+    { name: "clockify" },
+    { name: "drive" },
+    { name: "chakra" },
+  ];
+  useEffect(() => {
+    setDataFetch(FakeData);
+  }, []);
+
+  return (
+    <>
+      {DataFetch?.length &&
+        DataFetch.map((el, index) => (
+          <HStack
+            key={index}
+            bg="#fff"
+            justifyContent={"space-between"}
+            pr="14"
+          >
+            <Flex p="3" alignItems={"center"} gap={2}>
+              <Checkbox />
+              <Text>{el.name}</Text>
+              <UpDownIcon fontSize={"12"} />
+            </Flex>
+
+            <Box display={["none", "none", "block"]}>
+              <Flex p="3" alignItems={"center"} gap={2}>
+                <Text>CLIENT</Text>
+                <UpDownIcon fontSize={"12"} />
+              </Flex>
+            </Box>
+            <Box display={["none", "none", "block"]}>
+              <Flex p="3" alignItems={"center"} gap={2}>
+                <Text>AMOUNT</Text>
+                <UpDownIcon fontSize={"12"} />
+              </Flex>
+            </Box>
+            <Box display={["none", "none", "block"]}>
+              {" "}
+              <Flex p="3" alignItems={"center"} gap={2}>
+                <Text>PROGRESS</Text>
+                <UpDownIcon fontSize={"12"} />
+              </Flex>
+            </Box>
+
+            <Box display={["none", "none", "block"]}>
+              <Flex p="3" alignItems={"center"} gap={2}>
+                <Text>ACCESS</Text>
+                <UpDownIcon fontSize={"12"} />
+              </Flex>
+            </Box>
+            <Box display={["block", "block", "none"]}>
+              <ChevronDownIcon />
+            </Box>
+          </HStack>
+        ))}
+    </>
+  );
+};
